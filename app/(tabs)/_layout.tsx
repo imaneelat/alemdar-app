6f471c9ae933b4288a9242194df2e1adc25cf1ce
Imane El Atmani <imaneelatmani06@icloud.com>
2026-06-18 22:09:21 +0300

    2nd test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import React from "react";
+import { Pressable } from "react-native";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { useColorScheme } from "@/components/useColorScheme";
+import Colors from "@/constants/Colors";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -19,19 +19,19 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-       
     <Tabs
       screenOptions={{
-       tabBarActiveTintColor: colorScheme === 'dark' ? '#808080' : '#000000',
-tabBarInactiveTintColor: colorScheme === 'dark' ? '#808080' : '#000000',
+        tabBarActiveTintColor: colorScheme === "dark" ? "#808080" : "#000000",
+        tabBarInactiveTintColor: colorScheme === "dark" ? "#808080" : "#000000",
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -40,7 +40,7 @@ tabBarInactiveTintColor: colorScheme === 'dark' ? '#808080' : '#000000',
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -52,23 +52,18 @@ tabBarInactiveTintColor: colorScheme === 'dark' ? '#808080' : '#000000',
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Search',
+          title: "Search",
           tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
         }}
       />
 
-    <Tabs.Screen
-      name="three"
-      options={{
-        title: 'Account',
-       tabBarIcon: ({ color }) => (
-      <TabBarIcon name="user" color={color} />
-    ),
-  }}
-/>
+      <Tabs.Screen
+        name="three"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
     </Tabs>
-    
-    
   );
-
-}
+}  // isalah hbil 
